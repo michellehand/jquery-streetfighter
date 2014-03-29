0ebe161587e80f6b8ad7b89c96b5060101c0dc88
michellehand <michellehand@outlook.com>
2014-03-29 18:03:25 -0500

    FINAL

--- js/app.js
+++ js/app.js
@@ -25,6 +25,19 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).keydown(function(event) {
+		if(event.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-ready').show();
+		}
+	});
 	function playHadouken () {
 		$('#hadouken-sound')[0].volume = 0.5;
 		$('#hadouken-sound')[0].load();
